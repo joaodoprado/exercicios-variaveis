0aee716045c5873ae7654370411fef801c7a97ca
Joao <jv.prado98@gmail.com>
2023-07-19 14:21:11 -0300

    ok

--- FahrenheitxCelsius.js
+++
@@ -1,4 +0,0 @@
-let fahrenheit = 86;
-let celsius = (fahrenheit - 32) / 1.8;
-
-console.log(`${fahrenheit}° Fahrenheit em celsius são ${celsius}°`)
--- IMC.js
+++
@@ -1,6 +0,0 @@
-let peso = 110;
-let altura = 1.84;
-
-const IMC = 100 / (1.84 * 1.84);
-
-console.log(`Seu IMC é ${IMC}`)
