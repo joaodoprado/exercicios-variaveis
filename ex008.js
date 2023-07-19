ca9702d2053723f24b442332318b4f529a072c1b
Joao <jv.prado98@gmail.com>
2023-07-19 16:48:18 -0300

    ex008

---
+++ ex008.js
@@ -0,0 +1,6 @@
+// Calculo do perímetro e área de um círculo
+let R = 5;
+const C = 2 * Math.PI * R ;
+const Ao = Math.PI * R**2;
+
+console.log(`O perímetro de um círculo com Raio de ${R}cm é de ${C.toFixed(1)}cm e a Área é de ${Ao.toFixed(1)}cm`)
