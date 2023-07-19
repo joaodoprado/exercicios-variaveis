b4276aeb85f5d95fa131cf560b715f3ee6aabc4c
Joao <jv.prado98@gmail.com>
2023-07-19 16:58:24 -0300

    ola

---
+++ ex009.js
@@ -0,0 +1,12 @@
+// Soma dos ângulos internos de um polígono
+
+// A = valor dos ângulos
+
+// n = número de lados do polígono
+let n = 6
+let S = (n - 2) * 180
+
+let A = S / n
+
+
+console.log(`A soma dos ${n} lados do polígono é igual a ${S}° e o valor dos seus ângulos é de ${A}°.`)
